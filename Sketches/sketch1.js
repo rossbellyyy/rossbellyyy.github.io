f90f9882b48be8b75b7a5af3212c4f1f6a90ff77
rossbellyyy <126787576+rossbellyyy@users.noreply.github.com>
2023-05-25 08:22:18 -0400

    Add files via upload

---
+++ Sketches/sketch1.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill("tan")
+  ellipse(200,200,200,300)
+
+  fill('white')
+  ellipse(250,150,50,50)
+  ellipse(150,150,50,50)
+  
+  fill('black')
+  ellipse(150,150,15,50)
+  ellipse(250,150,15,50)
+  
+  rect(225,110,50,10)
+}
